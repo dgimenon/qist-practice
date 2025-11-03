55edd153ca2289ada75349ba780a90a40b2959fc
Ruaridh Morgan <rm846@sussex.ac.uk>
2025-11-03 16:17:53 +0000

    Made a change to my branch

--- test1.py
+++ test1.py
@@ -1,4 +1,2 @@
-print('hi ru')
-print('hi damian')
-print("I hope this gets easier lol ")
-print("I hope so too!")
+favourite_troop=input("What is your favourite clash troop Damian?")
+print("I think that", favourite_troop, "is rubbish!")
