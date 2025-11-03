1e16313c92c3e080bc80d92034ead36bdaca3aea
G-money-coding <gh382@sussex.ac.uk>
2025-11-03 16:37:53 +0000

    commiting secret sauce

--- test1.py
+++ test1.py
@@ -1,4 +1,10 @@
+import multiprocessing
+import time
 print('hi ru')
 print('hi damian')
 print("I hope this gets easier lol ")
 print("I hope so too!")
+print("watch me count to 100!")
+for i in range(0,100,1):
+    print(i+1)
+    time.sleep(1)
